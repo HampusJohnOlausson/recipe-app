b08dc6b938b208d21291c803ee81bc0153b245a9
Hampus Olausson <hampus.john.olausson@gmail.com>
2021-02-18 10:15:38 +0100

    async await fetch

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import React, {useEffect, useState} from 'react';
 
@@ -6,14 +5,16 @@ function App() {
 
   const APP_ID = '3a547a51';
   const APP_KEY = 'e98a86b97809066fae951c6fdbac1abf';
-  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
-
-
-  const [counter, setCounter] = useState(0);
 
   useEffect(() => {
-    console.log('effect has been run');
-  });
+    getRecipes();
+  }, []);
+
+  const getRecipes = async () => {
+    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const data = await response.json();
+    console.log(data);
+  };
 
   return (
     <div className="App">
@@ -21,7 +22,6 @@ function App() {
       <input className="searchInput" type="text" />
       <button className="searchBtn" type="submit" >Search</button>
     </form>
-    <h1 onClick={() => setCounter(counter + 1)}>{counter}</h1>
     </div>
   );
 }
