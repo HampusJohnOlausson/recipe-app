236a0da0f773fb26b3b75d704e28c74c6802a8f1
Hampus Olausson <hampus.john.olausson@gmail.com>
2021-02-18 10:02:37 +0100

    added api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useEffect, useState} from 'react';
 
 function App() {
+
+  const APP_ID = '3a547a51';
+  const APP_KEY = 'e98a86b97809066fae951c6fdbac1abf';
+  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
+
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    console.log('effect has been run');
+  });
+
   return (
     <div className="App">
-    <h1>Hello React</h1>
+    <form className="searchForm" >
+      <input className="searchInput" type="text" />
+      <button className="searchBtn" type="submit" >Search</button>
+    </form>
+    <h1 onClick={() => setCounter(counter + 1)}>{counter}</h1>
     </div>
   );
 }
