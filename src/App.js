325d1861da82f88c1191078bd0f779cb62737798
Hampus Olausson <hampus.john.olausson@gmail.com>
2021-02-18 10:26:23 +0100

    Fetched the title, calories, image

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import './App.css';
 import React, {useEffect, useState} from 'react';
+import Recipe from './Recipe';
 
 function App() {
 
   const APP_ID = '3a547a51';
   const APP_KEY = 'e98a86b97809066fae951c6fdbac1abf';
 
+  const [recipes, setRecipes] = useState([]);
+
   useEffect(() => {
     getRecipes();
   }, []);
 
   const getRecipes = async () => {
-    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const response = await fetch(
+      `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+    );
     const data = await response.json();
-    console.log(data);
+    setRecipes(data.hits);
+    
   };
 
   return (
@@ -22,6 +28,12 @@ function App() {
       <input className="searchInput" type="text" />
       <button className="searchBtn" type="submit" >Search</button>
     </form>
+    {recipes.map(recipe => (
+      <Recipe 
+      title={recipe.recipe.label} 
+      calories={recipe.recipe.calories} 
+      image={recipe.recipe.image}/>
+    ))}
     </div>
   );
 }
